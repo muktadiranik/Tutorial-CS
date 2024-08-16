5b8b171b260cb6b0fb7aff4408e639ac8489e2b4
muktadiranik <muktadiranik@gmail.com>
2024-08-16 23:39:40 +0600

    Conditionals While Exceptions

--- Conditionals While Exceptions/obj/Debug/net8.0/Conditionals While Exceptions.AssemblyInfo.cs
+++ Conditionals While Exceptions/obj/Debug/net8.0/Conditionals While Exceptions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Conditionals While Exceptions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7959a65339d53571ff0c8fe34693ad1ea01c7612")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+376940ed2a19a1a7fed314a5915315e82e6a56cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Conditionals While Exceptions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Conditionals While Exceptions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
